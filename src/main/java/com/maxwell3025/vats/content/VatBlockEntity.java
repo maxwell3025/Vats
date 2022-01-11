8f2ce245051846c087880d5ceac32b6ec43a7a11
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2022-01-11 15:32:07 -0500

    added data saving code

--- src/main/java/com/maxwell3025/vats/content/VatBlockEntity.java
+++ src/main/java/com/maxwell3025/vats/content/VatBlockEntity.java
@@ -2,10 +2,12 @@ package com.maxwell3025.vats.content;
 
 import com.maxwell3025.vats.AnnotatedHolder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
 public class VatBlockEntity extends BlockEntity {
     private static final Logger LOGGER = LogManager.getLogger();
@@ -15,8 +17,19 @@ public class VatBlockEntity extends BlockEntity {
     }
     public void increment(){
         clickTimes++;
+        this.setChanged();
     }
     public int getClickTimes(){
         return clickTimes;
+    }
+    @Override
+    public void saveAdditional(CompoundTag tag){
+        super.saveAdditional(tag);
+        tag.putInt("clickTimes",clickTimes);
+    }
+    @Override
+    public void load(CompoundTag tag){
+        super.load(tag);
+        clickTimes = tag.getInt("clickTimes");
     }
 }
